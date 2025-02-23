74d0859fbd786601322dbab7676be927e07186d3
anya768 <anyasingh070605@gmail.com>
2025-02-23 15:54:34 -0800

    fixed issues

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -89,6 +89,18 @@
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	(() => {
 /******/ 		// define getter functions for harmony exports
@@ -117,7 +129,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("f8cbf1090c0fca18")
+/******/ 		__webpack_require__.h = () => ("975c0374aeaedf38")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
